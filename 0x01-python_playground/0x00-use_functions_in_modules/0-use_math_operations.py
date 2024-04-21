8e9451080cd11de68e3fc8377bc25fee08ecf17c
wikki2000 <wisdomokposin@gmail.com>
2024-04-21 10:43:02 -0700

    Update Repo

---
+++ 0x01-python_playground/0x00-use_functions_in_modules/0-use_math_operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Use the addition function from modules/1-math_operations.py"""
+add_sibling_to_path = __import__("1-path_helper").add_sibling_to_path
+# This is use to add modules directory to python path
+# This is done to to import modules from siblings directory
+add_sibling_to_path("modules")
+
+from math_operations import addition
+
+result = addition(5, 3)
+print("The sum is:", result)
+
