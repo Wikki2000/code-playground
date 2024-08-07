c32f8c1886f2f9b656e05e762d2dba284afcae0f
wikki2000 <wisdomokposin@gmail.com>
2024-08-07 14:57:00 +0100

    update

--- python/0-use_math_operations.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-"""Use the addition function from math_operations.py"""
-from modules import math_operations
-
-result = math_operations.addition(5, 3)
-print("The sum is:", result)
-
--- python/1-use_strlen_from_attribute.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from modules.strlen_from_attribute import String
-
-obj = String("Wisdom")
-print(obj.strlen)
