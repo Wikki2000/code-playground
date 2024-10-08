29bfcee44d4f3dc3e2711090deae6c7046f2083a
wikki2000 <wisdomokposin@gmail.com>
2024-10-08 22:16:10 +0100

    Update

---
+++ nodejs/array_dict/4-each.js
@@ -0,0 +1,6 @@
+const users = ['John', 'Jane', 'Jack'];
+
+// Using $.each to iterate through array
+$.each(users, function(index, value) {
+  console.log(`${index + 1}: ${value}`);
+});
---
+++ nodejs/array_dict/4-forEach.js
@@ -0,0 +1,7 @@
+const users = ['John', 'Jane', 'Jack'];
+
+// Using forEach to iterate through array
+users.forEach((user, index) => {
+  console.log(`${index + 1}: ${user}`);
+});
+
