97062bf92c0a114dd0a0aada01962ee2e1f9b0d4
wikki2000 <wisdomokposin@gmail.com>
2024-10-11 10:56:47 +0100

    Update: 4-object_unpacking.js

--- nodejs/array_dict/4-object_unpacking.js
+++ nodejs/array_dict/4-object_unpacking.js
@@ -1,5 +1,5 @@
 object ={ name: 'Wisdom Okposin', state: 'Akwa-ibom' };
 
-const {name, state} = object;
+const { name, state } = object;
 console.log('First item unpack (Name):', name);
 console.log('second item unpack (State of Origin):', state);
